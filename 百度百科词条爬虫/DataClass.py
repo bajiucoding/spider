cd06623f1b90f153939a437a0d69d5c5a4a7a4dd
gongyi <gxylogin@sina.com>
2019-07-11 23:32:29 +0800

    本机模拟实现分布式爬虫

--- 百度百科词条爬虫/DataClass.py
+++ 百度百科词条爬虫/DataClass.py
@@ -5,21 +5,35 @@
 
 #数据存储器
 import codecs
+import time
 
 class DataClass():
     def __init__(self):
+        self.filepath = 'baike_%s.html'%(time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime()))
+        self.outputHead(self.filepath)
         self.datas = []
 
     def store_data(self,data):
         if not data:
             return
         self.datas.append(data)
+        if len(self.datas)>10:
+            self.outputHTML(self.filepath)
 
-    def outputHTML(self):
-        fout = codecs.open('baike.html','w',encoding='utf-8')
+    def outputHead(self,path):
+        '''
+        将HTML头写进去
+        '''
+        fout = codecs.open(path,'w',encoding='utf-8')
         fout.write('<html>')
         fout.write('<body>')
         fout.write('<table>')
+        fout.close()
+
+
+    def outputHTML(self,path):
+        '''将数据写入HTML文件中'''
+        fout = codecs.open(path,'a',encoding='utf-8')
         for data in self.datas:
             fout.write('<tr>')
             fout.write('<td>%s</td>'%data['url'])
@@ -27,7 +41,13 @@ class DataClass():
             fout.write('<td>%s</td>'%data['summary'])
             fout.write('<tr>')
             self.datas.remove(data)
-        fout.write('</html>')
-        fout.write('</body>')
+        fout.close()
+
+    def outputEnd(self,path):
+        '''
+        输出HTML结尾标志'''
+        fout = codecs.open(path,'a',encoding='utf-8')
         fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
         fout.close()
--- 百度百科词条爬虫/URLManager.py
+++ 百度百科词条爬虫/URLManager.py
@@ -5,6 +5,8 @@
 
 #url管理器
 import requests
+import pickle
+import hashlib
 
 class URLManager():
 
@@ -24,7 +26,10 @@ class URLManager():
         '''
         if not url:
             return None
-        if url not in self.new_urls and url not in self.old_urls:
+        m = hashlib.md5()
+        m.update(url.encode())
+        url_md5 = m.hexdigest()[8:-8]
+        if url not in self.new_urls and url_md5 not in self.old_urls:
             print(url)
             self.new_urls.add(url)
 
@@ -33,15 +38,17 @@ class URLManager():
         if len(urls)==0:
             return None
         for url in urls:
-            if url not in self.new_urls and url not in self.old_urls:
-                self.new_urls.add(url)
+            self.add_new_url(url)
 
     def get_new_url(self):
         '''
         获取一个未爬取的url
         '''
         new_url = self.new_urls.pop()
-        self.old_urls.add(new_url)
+        #此时要对已爬取过的url做md5处理，减少url长度，便于存储
+        m = hashlib.md5()
+        m.update(new_url.encode())
+        self.old_urls.add(m.hexdigest()[8:-8])
         return new_url
 
     def new_url_size(self):
@@ -54,3 +61,27 @@ class URLManager():
         #获取已爬取url集合的大小
         return len(self.old_urls)
 
+
+    def store(self,path,data):
+        '''
+        保存url的爬取进度
+        :in param path:文件路径
+        :in param data:待存储数据，这里是url集合
+        '''
+        with open(path,'ab') as f:
+            pickle.dump(data,f)
+
+    def load(self,path):
+        '''
+        从文件中加载进度
+        :in param path:文件路径
+        :return:返回set集合
+        '''
+        print('[+]从文件加载进度：%s'%path)
+        try:
+            with open(path,'rb') as f:
+                data = pickle.load(f)
+                return data
+        except:
+            print('[!]无进度文件，请检查%s'%path)
+        return set()
